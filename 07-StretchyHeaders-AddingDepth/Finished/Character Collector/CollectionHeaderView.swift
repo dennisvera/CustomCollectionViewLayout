50c25760e6c3aa4b7a9193c4416f9c731add9228
dennisvera <dennisvera.z@gmail.com>
2017-05-12 22:16:36 -0400

    adding depth to stretchy header

---
+++ 07-StretchyHeaders-AddingDepth/Finished/Character Collector/CollectionHeaderView.swift
@@ -0,0 +1,66 @@
+//
+//  CollectionHeaderView.swift
+//  Character Collector
+//
+/*
+ * Copyright (c) 2016 Razeware LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+import UIKit
+
+class CollectionHeaderView: UICollectionReusableView {
+  @IBOutlet weak var backgroundImage: UIImageView! 
+  @IBOutlet weak var foregroundImage: UIImageView!
+  
+  @IBOutlet weak var backgroundImageHeightConstraint: NSLayoutConstraint!
+  @IBOutlet weak var foregroundImageHeightConstraint: NSLayoutConstraint!
+  
+  var backgroundImageHeight: CGFloat = 0
+  var foregroundImageHeight: CGFloat = 0
+  var previousHeight: CGFloat = 0
+  
+  override func awakeFromNib() {
+    super.awakeFromNib()
+    backgroundImageHeight = backgroundImage.bounds.height
+    foregroundImageHeight = foregroundImage.bounds.height
+  }
+  
+  override func apply(_ layoutAttributes: UICollectionViewLayoutAttributes) {
+    super.apply(layoutAttributes)
+    let attributes = layoutAttributes as! StretchyHeaderAttributes
+    
+    let height = (attributes.frame).height
+    if previousHeight != height {
+      backgroundImageHeightConstraint.constant = backgroundImageHeight - attributes.deltaY
+      foregroundImageHeightConstraint.constant = foregroundImageHeight + attributes.deltaY
+      previousHeight = height
+    }
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+}
---
+++ 07-StretchyHeaders-AddingDepth/Starter/Character Collector/StretchyHeaderLayout.swift
@@ -0,0 +1,81 @@
+//
+//  StretchyHeaderLayout.swift
+//  Character Collector
+//
+/*
+ * Copyright (c) 2016 Razeware LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+import UIKit
+
+class StretchyHeaderAttributes: UICollectionViewLayoutAttributes {
+    
+    var deltaY: CGFloat = 0
+    
+    override func copy(with zone: NSZone?) -> Any {
+        let copy = super.copy(with: zone) as! StretchyHeaderAttributes
+        copy.deltaY = deltaY
+        return copy
+    }
+    
+    override func isEqual(_ object: Any?) -> Bool {
+        if let attributes = object as? StretchyHeaderAttributes {
+            if attributes.deltaY == deltaY {
+                return super.isEqual(object)
+            }
+        }
+        return false
+    }
+}
+
+class StretchyHeaderLayout: UICollectionViewFlowLayout {
+    
+    var maximumStretchHeight: CGFloat = 0
+    
+    override class var layoutAttributesClass : AnyClass {
+        return StretchyHeaderAttributes.self
+    }
+    
+    override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
+        let layoutAttributes = super.layoutAttributesForElements(in: rect)! as! [StretchyHeaderAttributes]
+        let offset = collectionView!.contentOffset
+        if (offset.y < 0) {
+            let deltaY = fabs(offset.y)
+            for attributes in layoutAttributes {
+                if let elementKind = attributes.representedElementKind {
+                    if elementKind == UICollectionElementKindSectionHeader {
+                        var frame = attributes.frame
+                        frame.size.height = min(max(0, headerReferenceSize.height + deltaY), maximumStretchHeight)
+                        frame.origin.y = frame.minY - deltaY
+                        attributes.deltaY = deltaY
+                        attributes.frame = frame
+                    }
+                }
+            }
+        }
+        return layoutAttributes
+    }
+    
+    override func shouldInvalidateLayout(forBoundsChange newBounds: CGRect) -> Bool {
+        return true
+    }
+}
+
